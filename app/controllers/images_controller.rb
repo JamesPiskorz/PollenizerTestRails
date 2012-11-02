b5d9b50ff905d80c7fa3e8ae89c8526ca63ac01b
James Piskorz <james@omnomnomstudios.com>
2012-11-02 15:25:47 +1100

    Mainly formatting changes. some indents were wrong.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,24 +1,23 @@
 class ImagesController < ApplicationController
   # GET /images
   def index
-
   end
 
   # GET /images/1
   def show
     @image = Image.find(params[:id])
-   
-  end
-  # GET images/search?q=hey&commit=Search
-  def search
-   # i feel this is a little hacky... can the page be nil checked in the paginate method
-   if params[:page].nil? then
-     params[:page]=1
-   end
-   
-   @images_paginated = Image.paginate(params[:q],params[:page])
-   @query = params[:q] 
- 
   end
 
- end
+  # GET images/search?q=hey&commit=Search
+  def search
+    #if the page is not specified then we assume we're showing the first page.
+    if params[:page].nil? then
+      params[:page]=1
+    end
+
+    @images_paginated = Image.paginate(params[:q],params[:page])
+    @query = params[:q]
+
+  end
+
+end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,32 +1,33 @@
-
+#Our image does NOT inherite from active record because we dont save to the DB infact there is no persistance to it
+#instead we will implement several parts of the ActieModel allow some functionality out of the box.
 class Image
 
- include ActiveModel::Validations
- include ActiveModel::Conversion
-
- extend ActiveModel::Naming
-
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+  
   def persitant?
     return false
   end
- 
+
   def self.all
     @images = {}
   end
-  
+
   def self.find(id)
-     @image = flickr.photos.getSizes(:photo_id=>id)
+    @image = flickr.photos.getSizes(:photo_id=>id)
   end
-  
+
   def self.search(searchterm="", page=1)
     @images = flickr.photos.search(:text=>searchterm, :page=>page, :per_page=>10, :extras=>"url_sq,url_o")
   end
-  
+
   def self.paginate(searchterm="", page=1)
     imagelist = search(searchterm,page)
-    # we use the will_paginate gem to manage all of our pagination. Except we are responsible for fetching new data. 
+    # we use the will_paginate gem to manage all of our pagination. Except we are responsible for fetching new data.
     @images_paginated = WillPaginate::Collection.create(page, 10, imagelist.total) do |pager|
       pager.replace(imagelist.to_a[0, 10])
     end
   end
+  
 end
