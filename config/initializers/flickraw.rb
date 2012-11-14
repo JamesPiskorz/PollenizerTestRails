3ab74fabb6dbb57b08a7db557ced651c2814eb26
James Piskorz <james@omnomnomstudios.com>
2012-11-15 08:46:48 +1100

    Initalizer name change (old api name was used)

---
+++ config/initializers/flickraw.rb
@@ -0,0 +1,5 @@
+# Setup the FlickRaw Flickr api information. these are set to this value for all environments
+
+
+FlickRaw.api_key="1530ff6c590d0bf02f3f75f5e4f02290"
+FlickRaw.shared_secret ="2ef71d4e3be3e437"
